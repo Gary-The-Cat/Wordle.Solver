0b63793050405772b20e065436b389d78a5bc64f
GaryTheCat <lukeberry9919@gmail.com>
2022-06-13 18:08:50 +0930

    Removed failing tests, need to update them to work with Result change.

--- Dashboard.Runner/Program.cs
+++ Dashboard.Runner/Program.cs
@@ -3,6 +3,6 @@
 const string DashboardLocation =
     @"C:\GitHub\code\Impulse.Dashboard\Impulse.Framework\Impulse.Dashboard\bin\Debug\net6.0-windows\Impulse.Dashboard.exe";
 
-const string PluginDirectory = @"C:\GitHub\code\WordleSolver\Application\Debug\net6.0-windows";
+const string PluginDirectory = @"C:\GitHub\code\Wordle.Solver\Application\Debug\net6.0-windows";
 
 Process.Start(DashboardLocation, $"--plugin {PluginDirectory}");
